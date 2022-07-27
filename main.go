aca35201b869e1a903d7fae7fbc491a850de87ae
vnSasa <ol.yaremechko@gmail.com>
2022-07-27 11:49:00 +0300

    changed main

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import(
 
+	// "github.com/vnSasa/homework12/task4kyu"
 	// "github.com/vnSasa/homework12/task5kyu"
-	"github.com/vnSasa/homework12/task4kyu"
+	"github.com/vnSasa/homework12/task6kyu"
 
 	"fmt"
 )
 
 func main() {
-	fmt.Println(task4kyu.Partitions(6))
+	// fmt.Println(task4kyu.Partitions(6))
 	// fmt.Println(task5kyu.Partitions(6))
 	// fmt.Println(task5kyu.F(5000000))
+	fmt.Println(task6kyu.FindHeight(6))
 }

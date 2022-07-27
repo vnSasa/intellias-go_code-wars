262f699e607a641c603e53e66f2dcd97d5f080fe
vnSasa <ol.yaremechko@gmail.com>
2022-07-27 11:48:13 +0300

    added "The Pyramid of Cubes"

---
+++ task6kyu/pyramidOfCubes.go
@@ -0,0 +1,24 @@
+package task6kyu
+
+// The Pyramid of Cubes
+// https://www.codewars.com/kata/61707b71059070003793bc0f
+
+func FindHeight(cubes int) int {
+	if cubes == 0 {
+		return 0
+	}
+	layer := 2
+	used := 1
+	n := 1
+	for i := 0; ; i++ {
+		used += n+layer
+		if used == cubes {
+			return layer
+		}
+		if used > cubes {
+			return layer-1
+		}
+		n+=layer
+		layer++
+	}
+}

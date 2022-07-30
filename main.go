dd0f06861f7909f85a444047dcee2f1fadb50b02
vnSasa <ol.yaremechko@gmail.com>
2022-07-30 06:16:59 +0300

    added "convert string to camel case"

--- main.go
+++ main.go
@@ -4,27 +4,30 @@ import(
 
 	// "github.com/vnSasa/homework12/task4kyu"
 	// "github.com/vnSasa/homework12/task5kyu"
-	// "github.com/vnSasa/homework12/task6kyu"
-	"github.com/vnSasa/homework12/task7kyu"
+	"github.com/vnSasa/homework12/task6kyu"
+	// "github.com/vnSasa/homework12/task7kyu"
 
 	"fmt"
 )
 
 func main() {
 	
-	// fmt.Println(task4kyu.Partitions(6))				// Number of integer partitions
+	// fmt.Println(task4kyu.Partitions(6))					// Number of integer partitions
 	
-	// fmt.Println(task5kyu.Partitions(6))				// Number of integer partitions with distinct parts
+	// fmt.Println(task5kyu.Partitions(6))					// Number of integer partitions with distinct parts
 	
-	// fmt.Println(task5kyu.F(5000000))				// Primes with Even Digits
+	// fmt.Println(task5kyu.F(5000000))					// Primes with Even Digits
 	
-	// fmt.Println(task6kyu.FindHeight(6))				// The Pyramid of Cubes
+	// fmt.Println(task6kyu.FindHeight(6))					// The Pyramid of Cubes
 	
-	// fmt.Println(task6kyu.Sum(132189))				// Sum of Digits / Digital Root
+	// fmt.Println(task6kyu.Sum(132189))					// Sum of Digits / Digital Root
 	
-	// slice1 := []int{1,2,2,2,3}					// Array.diff
-	// slice2 := []int{2}						// Array.diff
-	// fmt.Println(task6kyu.ArrayDiff(slice1, slice2))		// Array.diff
+	// slice1 := []int{1,2,2,2,3}						// Array.diff
+	// slice2 := []int{2}							// Array.diff
+	// fmt.Println(task6kyu.ArrayDiff(slice1, slice2))			// Array.diff
 
-	fmt.Println(task7kyu.Solve("123456", 2))			// Simple string division
+	// fmt.Println(task7kyu.Solve("123456", 2))				// Simple string division
+
+	fmt.Println(task6kyu.ToCamelCase("the-stealth-warrior"))	// Convert string to camel case
+	fmt.Println(task6kyu.ToCamelCase("The_Stealth_Warrior"))	// Convert string to camel case
 }
---
+++ task6kyu/stringToCamelCase.go
@@ -0,0 +1,29 @@
+package task6kyu
+
+// Convert string to camel case
+// https://www.codewars.com/kata/517abf86da9663f1d2000003
+
+import (
+	"strings"
+	"bytes"
+)
+
+func ToCamelCase(s string) string {
+	var str bytes.Buffer
+	var do bool
+
+	for _, ch := range s {
+		if ch != '_' && ch != '-' {
+			if do {
+				ch = rune(strings.ToUpper(string(ch))[0])
+			}
+
+			str.WriteRune(ch)
+			do = false
+		} else {
+			do = true
+		}
+	}
+
+	return str.String()
+}
